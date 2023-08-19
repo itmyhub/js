d547e376dce1cc5082435d08a59f99c3ff6dae9e
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 06:35:33 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // This targets buttons with the specific onclick attribute
     var buttons = document.querySelectorAll('button[onclick^="location.href=\'https://v1-generatepress.1309816.xyz/contact-us/\'"]');
 
     buttons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
-            window.open('https://v1-generatepress.1309816.xyz/contact-us/', '_blank');
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default click action
+            window.open('https://v1-generatepress.1309816.xyz/contact-us/', '_blank'); // Open the link in a new tab
         });
     });
 });
