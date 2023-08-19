aeb5f9f8ca67baa859ae1ee9139e527c8b382e23
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 05:45:55 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -1,9 +1,16 @@
-jQuery(document).ready(function($) {
-    $("a").each(function() {
-        var url = $(this).attr('href');
-        var text = $(this).text();
-        if (url !== "#content") {  // Exclude links with href="#content"
-            $(this).replaceWith('<button onclick="location.href=\'' + url + '\'">' + text + '</button>');
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all anchor links (or you can modify the selector to select specific links)
+    let links = document.querySelectorAll('a');
+
+    links.forEach(function(link) {
+        // Create a new button element
+        let button = document.createElement('button');
+        button.innerHTML = link.innerHTML;
+        button.onclick = function() {
+            location.href = link.href;
+        };
+
+        // Replace the link with the button
+        link.parentNode.replaceChild(button, link);
     });
 });
