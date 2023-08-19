7db7fd9c3db05042feeeb85e7ca588948c0ce43e
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 05:43:53 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function($) {
     $("a").each(function() {
         var url = $(this).attr('href');
         var text = $(this).text();
-        $(this).replaceWith('<button onclick="location.href=\'' + url + '\'">' + text + '</button>');
+        if (url !== "#content") {  // Exclude links with href="#content"
+            $(this).replaceWith('<button onclick="location.href=\'' + url + '\'">' + text + '</button>');
+        }
     });
 });
