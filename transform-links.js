767a0a6d9dc4d7ff9012d1a18e40b139a3e46d48
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 06:03:46 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -1,18 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var anchors = document.querySelectorAll('a');
-    
-    anchors.forEach(function(anchor) {
-        var href = anchor.getAttribute('href');
-        var text = anchor.textContent;
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a');
 
-        // Create a new button
-        var button = document.createElement('button');
-        button.textContent = text;
-        button.onclick = function() {
-            window.location.href = href;
-        };
-
-        // Replace the anchor with the button
-        anchor.parentNode.replaceChild(button, anchor);
+    links.forEach(function(link) {
+        if (!link.target) {
+            link.target = '_blank';
+        }
     });
 });
