b159a26555640409b634f57812df752ccd3f9586
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 06:17:10 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a');
+    // This targets only anchor tags with the href 'https://v1-generatepress.1309816.xyz/contact-us/'
+    var links = document.querySelectorAll('a[href="https://v1-generatepress.1309816.xyz/contact-us/"]');
 
     links.forEach(function(link) {
         if (!link.target) {
