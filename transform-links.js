c317a2a0e15d3179c1198e2c921ab3d5c2743c87
itmyhub <142577600+itmyhub@users.noreply.github.com>
2023-08-19 06:39:45 -0300

    Update transform-links.js

--- transform-links.js
+++ transform-links.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // This targets buttons with the specific onclick attribute
-    var buttons = document.querySelectorAll('button[onclick^="location.href=\'https://v1-generatepress.1309816.xyz/contact-us/\'"]');
+    // Remove the "Skip to content" button
+    var skipButtons = document.querySelectorAll('button:contains("Skip to content")');
+    skipButtons.forEach(function(button) {
+        button.style.display = 'none';
+    });
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default click action
-            window.open('https://v1-generatepress.1309816.xyz/contact-us/', '_blank'); // Open the link in a new tab
-        });
+    // Remove background color #55555e for all links
+    var linksWithBg = document.querySelectorAll('a[style*="background-color: #55555e"]');
+    linksWithBg.forEach(function(link) {
+        link.style.backgroundColor = 'transparent';
+    });
+
+    // Ensure the "Contact Us" button opens in a new tab
+    var contactUsButtons = document.querySelectorAll('button[onclick^="location.href=\'https://v1-generatepress.1309816.xyz/contact-us/\'"]');
+    contactUsButtons.forEach(function(button) {
+        button.onclick = function() {
+            window.open('https://v1-generatepress.1309816.xyz/contact-us/', '_blank');
+        };
     });
 });
